c66c0a9dc833ee931cf8508f435df795c9742b37
sukryucll <sukr.yucll.02@gmail.com>
2024-08-29 13:19:06 +0300

    v1_2

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react"
 import "./Quiz.css"
+import {data} from "../../assets/data"
 const Quiz = () => {
+
+  let[index,setIndex] = useState(2);
+  let[question,setQuestion] = useState(data[index]);
+  let[lock,setLock] = useState(false);
+
+  const checkAns = (e,ans) => {
+    if(lock===false){
+      if(question.ans===ans){
+        e.target.classList.add("correct");
+        setLock(true);
+      }
+      else{
+        e.target.classList.add("wrong");
+        setLock(true);
+      }
+    }
+  }
+
   return (
     <div className="container">
         <h1>Quiz App</h1>
         <hr/>
-        <h2>Which device is required for the Internet connection?</h2>
+        <h2>{index+1}.{question.question}</h2>
         <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>LAN Cable</li>
-            <li>Pen Drive</li>
+            <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+            <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+            <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+            <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5</div>
